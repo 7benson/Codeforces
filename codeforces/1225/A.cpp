fcf910a40f09147cfac5208b80017be9aca5cd68
JayanthKumar <jknani111@gmail.com>
2021-05-29 21:47:00 +0530

    Add solution for problem `A - Forgetting Things` Link: https://codeforces.com/contest/1225/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1225/submission/117794925

---
+++ codeforces/1225/A.cpp
@@ -0,0 +1,82 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+void solve(){
+}
+int main()
+{
+    ll a,b;
+    cin>>a>>b;
+    if((a-b)==-1){
+        if(a>b){
+            cout<<b<<"99 "<<a<<"00";
+        }else{
+            cout<<a<<"99 "<<b<<"00";
+        }
+        return 0;
+    }else if(a==b){
+        cout<<a<<"12 "<<b<<"13";
+        return 0;
+    }else{
+        ll i;
+        for(i=0;i<9;i++){
+            if(a+1==b*pow(10,i)){
+                cout<<a<<" "<<b*pow(10,i);
+                return 0;
+            }
+        }
+    }
+    cout<<"-1";
+    return 0;
+}
